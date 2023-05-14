0727eeaa86d4c9c47cbbe59baf37d56cfaca1e11
Gathan Mahesa <gthn217@gmail.com>
2023-05-14 23:29:26 +0700

    page is scrollable now

--- scroller.js
+++ scroller.js
@@ -1,13 +1,19 @@
 // Get the image container element
 const canvas = document.getElementById('canvas');
-const image_container = document.getElementById('image_container');
+const container = document.getElementById('container');
 
+let CURRENT_POSITION = 20
 // Listen for the wheel event
 canvas.addEventListener('wheel', (e) => {
+  const STARTING_POINT = 20;
+  const END_POINT = 320
+
   if (e.deltaY < 0){
-    console.log('scrollin up')
+    (CURRENT_POSITION--) <= 20 ? CURRENT_POSITION = 20 : CURRENT_POSITION-- 
+    console.log('scrollin up:' + CURRENT_POSITION);
   } else {
-    // image_container.style.transform = `transform: translateX(calc(-100% - 20vmin));`;
-    console.log('scrollin down')
+    (CURRENT_POSITION++) >= 320 ? CURRENT_POSITION = 320 : CURRENT_POSITION++
+    console.log('scrollin down:'+ CURRENT_POSITION);
   }
+  container.style.transform = `translateX(calc(50% - ${CURRENT_POSITION}vmin))`;
 });
