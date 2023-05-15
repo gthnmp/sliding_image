c5e502d5da78492813e8d1d17b3ca4fc5e284a72
Gathan Mahesa <gthn217@gmail.com>
2023-05-15 08:07:17 +0700

    Scrollable

--- scroller.js
+++ scroller.js
@@ -1,19 +1,27 @@
-// Get the image container element
 const canvas = document.getElementById('canvas');
 const container = document.getElementById('container');
 
 let CURRENT_POSITION = 20
-// Listen for the wheel event
+let PERCENTAGE = 0;
 canvas.addEventListener('wheel', (e) => {
   const STARTING_POINT = 20;
-  const END_POINT = 320
+  const END_POINT = 317.8;
+  const SCROLL_SPEED = 1;
 
+  // Movement
   if (e.deltaY < 0){
-    (CURRENT_POSITION--) <= 20 ? CURRENT_POSITION = 20 : CURRENT_POSITION-- 
+    (CURRENT_POSITION-=SCROLL_SPEED) <= STARTING_POINT ? CURRENT_POSITION = STARTING_POINT : CURRENT_POSITION-- 
     console.log('scrollin up:' + CURRENT_POSITION);
   } else {
-    (CURRENT_POSITION++) >= 320 ? CURRENT_POSITION = 320 : CURRENT_POSITION++
+    (CURRENT_POSITION+=SCROLL_SPEED) >= END_POINT ? CURRENT_POSITION = END_POINT : CURRENT_POSITION++
     console.log('scrollin down:'+ CURRENT_POSITION);
   }
   container.style.transform = `translateX(calc(50% - ${CURRENT_POSITION}vmin))`;
+
+  // Image Paralax
+  PERCENTAGE = CURRENT_POSITION / (END_POINT - STARTING_POINT);
+
+  for (const image of document.getElementById('image')){
+    image.style.object_position = `${PERCENTAGE}% center`;
+  }
 });
